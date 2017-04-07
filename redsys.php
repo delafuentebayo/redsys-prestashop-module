33c8e006d4a2c9dddf40c382ffae6e6ff479086c
GunaLogina <guna.logina@gmail.com>
2017-04-07 12:01:25 +0200

    Changed url when order is cancelled

--- redsys.php
+++ redsys.php
@@ -423,7 +423,7 @@ class Redsys extends PaymentModule
 		$miObj->setParameter("DS_MERCHANT_URLOK",$protocolo.$_SERVER['HTTP_HOST'].__PS_BASE_URI__.'index.php?controller=order-confirmation&id_cart='.$id_cart.'&id_module='.$this->id.'&id_order='.$this->currentOrder.'&key='.$customer->secure_key);
 
 		//http://localhost:8888/nuevo_prestashop/es/confirmacion-pedido?id_cart=11&id_module=26&id_order=10&key=e909865c31d029d3a685b6faabd5865e
-		$miObj->setParameter("DS_MERCHANT_URLKO",$protocolo.$_SERVER['HTTP_HOST'].__PS_BASE_URI__.'pedido');
+		$miObj->setParameter("DS_MERCHANT_URLKO",$protocolo.$_SERVER['HTTP_HOST'].__PS_BASE_URI__.'index.php?controller=order');
 		$miObj->setParameter("Ds_Merchant_ConsumerLanguage",$idioma_tpv);
 		$miObj->setParameter("Ds_Merchant_ProductDescription",$productos);
 		//$miObj->setParameter("Ds_Merchant_Titular",$this->nombre);
